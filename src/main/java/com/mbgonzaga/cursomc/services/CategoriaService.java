09ba22554ea056b1329680a7a49c9aa2bff46328
mateusbgonzaga1976 <mateusbgonzaga@gmail.com>
2023-08-05 16:31:28 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/mbgonzaga/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.mbgonzaga.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mbgonzaga.cursomc.domain.Categoria;
+import com.mbgonzaga.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Optional<Categoria> buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj;
+	}
+	
+}
